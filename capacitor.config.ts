3619e0982f1b41f1f0b5e4e177813cf0a665ef37
bube009 <alxfourn01@gmail.com>
2026-01-11 03:06:33 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Frequency Quantique v2',
+  webDir: 'dist'
+};
+
+export default config;
