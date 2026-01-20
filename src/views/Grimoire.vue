62de35af83e726c44d3c8e576e11d78271227896
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-20 18:26:53 -0500

    Add Grimoire.vue component with basic layout

---
+++ src/views/Grimoire.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Grimoire</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <p>Grimoire chargé.</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+</script>
