fb1497575e5302fde0a0e23cd0c5345f4498ac93
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-20 18:21:35 -0500

    Change title and content in Home.vue
    Updated the header title and modified the content structure.

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Frequency Quantique v2</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <p>Application chargée.</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+</script>
