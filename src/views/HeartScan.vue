c982f32de453dd38422175d0790b4413383ae56f
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-20 20:36:26 -0500

    Add HeartScan.vue component for heart scan view

---
+++ src/views/HeartScan.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Heart Scan</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <p>HeartScan chargé.</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+</script>
