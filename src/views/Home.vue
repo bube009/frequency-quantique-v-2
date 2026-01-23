1874582254acb0202ebb41c405a5bd72e39d71f1
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-23 17:02:48 -0500

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,3 +59,43 @@ const stopFrequency = () => {
   status.value = 'Fréquence arrêtée'
 }
 </script>
+import programs from '@/data/programs.json';
+
+let audioCtx = null;
+let oscillator = null;
+let timer = null;
+let index = 0;
+
+function playFrequency(freq) {
+  stopSound();
+  audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  oscillator = audioCtx.createOscillator();
+  oscillator.type = 'sine';
+  oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime);
+  oscillator.connect(audioCtx.destination);
+  oscillator.start();
+}
+
+function startProgram(program) {
+  index = 0;
+  const sliceDuration = (program.duration / program.frequencies.length) * 1000;
+
+  playFrequency(program.frequencies[index]);
+
+  timer = setInterval(() => {
+    index++;
+    if (index >= program.frequencies.length) {
+      stopSound();
+    } else {
+      playFrequency(program.frequencies[index]);
+    }
+  }, sliceDuration);
+}
+
+function stopSound() {
+  if (oscillator) oscillator.stop();
+  if (audioCtx) audioCtx.close();
+  oscillator = null;
+  audioCtx = null;
+  clearInterval(timer);
+}
