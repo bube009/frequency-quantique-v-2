7fe5853e03f26903ba46d965599ab1f63d8292cd
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-23 11:06:51 -0500

    Add iOS build number to capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.alexfournier.frequencyquantique',
   appName: 'Frequency Quantique',
-  webDir: 'dist'
+  webDir: 'dist',
+  ios: {
+    buildNumber: '2'
+  }
 };
 
 export default config;
