7f904a14c5f1fed2f4f81462ecfd33686af29f88
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-23 19:00:30 -0500

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -188,3 +188,16 @@ function startStressProgram() {
 
   playNext()
 }
+<style scoped>
+.stress-card {
+  margin-top: 20px;
+  padding: 16px;
+  border-radius: 16px;
+  color: white;
+  background: linear-gradient(
+    180deg,
+    #0a2a43,
+    #6ec6ff
+  );
+}
+</style>
