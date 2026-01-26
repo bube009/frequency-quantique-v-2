b7bcc02ca1b87200d4fb1ef3b91accfa9d0f5981
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-26 00:52:25 -0500

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,8 +7,19 @@ import HeartScan from '../views/HeartScan.vue';
 export default createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Home },
-    { path: '/grimoire', component: Grimoire },
-    { path: '/heart', component: HeartScan }
-  ]
-});
+    {
+  path: '/',
+  redirect: '/home',
+},
+{
+  path: '/home',
+  component: () => import('../views/Home.vue'),
+},
+{
+  path: '/pulse',
+  component: () => import('../views/Pulse.vue'),
+},
+{
+  path: '/sky',
+  component: () => import('../views/SkyCamera.vue'),
+}
