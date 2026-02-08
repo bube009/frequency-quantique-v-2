aca2a602c68204dc23e66147598212158dff5828
bube009 <207951757+bube009@users.noreply.github.com>
2026-02-07 21:11:43 -0500

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from '@ionic/vue-router'
 
 import Home from '../views/Home.vue'
+import Grimoire from '../views/Grimoire.vue'
+import HeartScan from '../views/HeartScan.vue'
 import Pulse from '../views/Pulse.vue'
 import SkyCamera from '../views/SkyCamera.vue'
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', redirect: '/home' },
+  { path: '/home', component: Home },
+  { path: '/grimoire', component: Grimoire },
+  { path: '/heart', component: HeartScan },
   { path: '/pulse', component: Pulse },
-  { path: '/camera', component: SkyCamera }
+  { path: '/sky', component: SkyCamera }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
