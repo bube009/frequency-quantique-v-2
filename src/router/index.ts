6f207373204ac2720144231754319c266a8564ed
bube009 <207951757+bube009@users.noreply.github.com>
2026-02-08 00:17:36 -0500

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,41 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router'
 
 import Home from '@/views/Home.vue'
-import Session from '@/views/Session.vue'
 import Pulse from '@/views/Pulse.vue'
 import SkyCamera from '@/views/SkyCamera.vue'
+import HeartScan from '@/views/HeartScan.vue'
+import Grimoire from '@/views/Grimoire.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/session', component: Session },
-  { path: '/pulse', component: Pulse },
-  { path: '/sky', component: SkyCamera }
+  {
+    path: '/',
+    redirect: '/home',
+  },
+  {
+    path: '/home',
+    component: Home,
+  },
+  {
+    path: '/pulse',
+    component: Pulse,
+  },
+  {
+    path: '/sky',
+    component: SkyCamera,
+  },
+  {
+    path: '/heart',
+    component: HeartScan,
+  },
+  {
+    path: '/grimoire',
+    component: Grimoire,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
 })
 
 export default router
