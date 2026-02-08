915420673756e1a3317adfa59150308f6829c097
bube009 <207951757+bube009@users.noreply.github.com>
2026-02-07 20:58:59 -0500

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router'
-import Home from '@/views/Home.vue'
-import Pulse from '@/views/Pulse.vue'
-import SkyCamera from '@/views/SkyCamera.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-export default createRouter({
+import Home from '../views/Home.vue'
+import Pulse from '../views/Pulse.vue'
+import SkyCamera from '../views/SkyCamera.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/pulse', component: Pulse },
+  { path: '/camera', component: SkyCamera }
+]
+
+const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: '/', component: Home },
-    { path: '/pulse', component: Pulse },
-    { path: '/sky', component: SkyCamera }
-  ]
+  routes
 })
+
+export default router
