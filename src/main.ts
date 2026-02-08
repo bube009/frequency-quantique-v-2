63827c56c9cca0623dd5ca4e84bac732dffad279
bube009 <207951757+bube009@users.noreply.github.com>
2026-02-07 21:49:13 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 import { IonicVue } from '@ionic/vue'
+
 import '@ionic/vue/css/core.css'
+import '@ionic/vue/css/normalize.css'
+import '@ionic/vue/css/structure.css'
+import '@ionic/vue/css/typography.css'
 
 createApp(App)
   .use(IonicVue)
