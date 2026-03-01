90411088db75c52d5f0a52b8406290ba057d0681
bube009 <207951757+bube009@users.noreply.github.com>
2026-02-28 19:29:50 -0500

    Change title and update SkyCamera layout
    Updated the title from 'Vision du ciel' to 'Sky Camera' and modified the content layout with a placeholder for future implementation.

--- src/views/SkyCamera.vue
+++ src/views/SkyCamera.vue
@@ -2,33 +2,25 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Vision du ciel</ion-title>
+        <ion-title>Sky Camera</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content class="bg">
-      <video ref="video" autoplay playsinline></video>
+    <ion-content class="bg" fullscreen>
+      <div class="center">
+        <p>Mode miroir et contraste à implémenter version native</p>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-
-const video = ref<HTMLVideoElement | null>(null)
-
-onMounted(async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true })
-  if (video.value) video.value.srcObject = stream
-})
-</script>
-
 <style scoped>
 .bg {
-  background: black;
+  --background: linear-gradient(180deg, #061d3a, #0a2a4d);
 }
-video {
-  width: 100%;
-  transform: scaleX(-1);
+.center {
+  text-align: center;
+  margin-top: 50px;
+  color: white;
 }
 </style>
