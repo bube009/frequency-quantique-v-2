99470f86964bf9a9401091e21e18b817f2c25a9c
bube009 <207951757+bube009@users.noreply.github.com>
2026-02-28 20:35:57 -0500

    Update router import from @ionic/vue-router to vue-routerC’est

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/views/Home.vue'
 import Session from '@/views/Session.vue'
