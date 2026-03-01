3ae3858ef45e13635f45ff099c8c64dadb27b8a8
bube009 <207951757+bube009@users.noreply.github.com>
2026-02-28 20:27:07 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import router from './router'
 import { IonicVue } from '@ionic/vue'
 
 import '@ionic/vue/css/core.css'
-import '@ionic/vue/css/normalize.css'
-import '@ionic/vue/css/structure.css'
-import '@ionic/vue/css/typography.css'
 
-createApp(App)
+const app = createApp(App)
   .use(IonicVue)
   .use(router)
-  .mount('#app')
+
+router.isReady().then(() => {
+  app.mount('#app')
+})
