6830675d0014df5416f6fc1bbbfff1b09f45d1c0
bube009 <207951757+bube009@users.noreply.github.com>
2026-02-28 20:34:20 -0500

    Import more Ionic CSS files for improved styling
    Added additional Ionic CSS imports for styling.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import router from './router'
 
 import { IonicVue } from '@ionic/vue'
 
+// Ionic CSS (essentiel)
 import '@ionic/vue/css/core.css'
+import '@ionic/vue/css/normalize.css'
+import '@ionic/vue/css/structure.css'
+import '@ionic/vue/css/typography.css'
+import '@ionic/vue/css/padding.css'
+import '@ionic/vue/css/flex-utils.css'
+import '@ionic/vue/css/text-alignment.css'
+import '@ionic/vue/css/text-transformation.css'
+import '@ionic/vue/css/display.css'
 
 const app = createApp(App)
   .use(IonicVue)
