100c64061d616fc47f4a12ed9f1aa4390360d568
bube009 <207951757+bube009@users.noreply.github.com>
2026-03-21 14:20:31 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import router from './router'
 
 import { IonicVue } from '@ionic/vue'
 
-// Ionic CSS (essentiel)
 import '@ionic/vue/css/core.css'
 import '@ionic/vue/css/normalize.css'
 import '@ionic/vue/css/structure.css'
