490273729fdcbb4d372d39a6a165268d5d8f1c5f
bube009 <207951757+bube009@users.noreply.github.com>
2026-03-30 23:00:40 -0400

    Add Session route to the router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import Home from '@/views/Home.vue'
+import Session from '@/views/Session.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +12,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/session/:id',
+    name: 'Session',
+    component: Session
   }
 ]
 
